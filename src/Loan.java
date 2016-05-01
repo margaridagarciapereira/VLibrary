a9c5b9dcd0b8a62be9afa4262060a99579650fee
rotiv695 <rotiv695@gmail.com>
2016-05-01 15:52:58 +0100

    class loan

---
+++ src/Loan.java
@@ -0,0 +1,37 @@
+import java.time.Instant;
+import java.util.Date;
+public class Loan {
+	private String loanId;
+	private Date date;
+	private Book book;
+	private Person person;
+	
+	/*public Loan(){
+		this.book = "";
+		
+	}*/
+	
+	public Loan(Book book, Person person){
+		this.book = book;
+		this.person = person;
+		this.date = Date.from(Instant.now());
+		this.loanId = person.GetName() + book.GetTitle();
+	}
+	
+	public Date GetDate(){
+		return this.date;
+	}
+	
+	public String GetLoanId(){
+		return this.loanId;
+	}
+	
+	public Book GetBook(){
+		return this.book;
+	}
+	
+	public Person GetPerson(){
+		return this.person;
+	}
+	
+}
